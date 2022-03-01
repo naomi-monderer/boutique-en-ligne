8b66103003e0988f773b2bd8e0cf3cac27746b51
lacroix <jerome.lacroix@laplateforme.io>
2022-03-01 11:08:07 +0100

    depot

---
+++ views/articles.php
@@ -0,0 +1,28 @@
+<?php
+$route =  str_replace('views/articles.php','',$_SERVER['SCRIPT_FILENAME']);
+require($route.'controllers/articlesController.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page article</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+
+
+    
+</body>
+</html>
