83980adf86eff67733555d8b715f43080f0d6fd7
lacroix <jerome.lacroix@laplateforme.io>
2022-03-07 07:17:53 +0100

    article fini

--- views/articles.php
+++ views/articles.php
@@ -2,7 +2,7 @@
 // $route =  str_replace('views/articles.php','',$_SERVER['SCRIPT_FILENAME']);
 // require($route.'controllers/articlesController.php');
 require_once('../controllers/articlesController.php');
-require_once('include/header.php')
+require_once('include/header.php');
 
 
 ?>
