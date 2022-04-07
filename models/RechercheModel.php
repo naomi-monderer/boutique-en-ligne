19d6a8dd84948b1c5a6a719dd0c83d3580961a00
sebastien-bargier <sebastien.bargier@laplateforme.io>
2022-04-07 11:09:27 +0200

    MAJ requete et affichage

--- models/RechercheModel.php
+++ models/RechercheModel.php
@@ -5,7 +5,7 @@ class Recherche extends Model
 {
     function rechercheProduitsBySearch($search)
     {
-        $requette= $this->connect()->prepare("SELECT * FROM produits WHERE titre LIKE '%".$search."%' ORDER BY id DESC");
+        $requette= $this->connect()->prepare("SELECT * , produits.id AS id_produit FROM `produits` INNER JOIN auteurs ON id_auteur = auteurs.id WHERE titre LIKE '%".$search."%' ORDER BY id_produit DESC");
         $requette->execute();
         $resultat = $requette->fetchAll(PDO::FETCH_ASSOC);
         return $resultat;
--- views/recherche.php
+++ views/recherche.php
@@ -23,7 +23,27 @@
 
         <?php foreach($_SESSION['search'] as $resultat) : ?>
 
-            <h3><?= $resultat['titre'] ?><h3>
+            <div class="articles">
+                <div class="articleDiv">
+                    <a href="article.php?id=<?php echo $resultat['id']?>"><img class="imgarticle" src="../picture/<?php echo $resultat["image"]; ?>" alt=""></a>
+                    
+                </div>
+                <div class="articleDivDescription">
+                     <a href="article.php?id=<?php echo $resultat["id"]?>"><p>Titre: <?php echo $resultat["titre"];  ?></p></a>
+                     <p>Auteur: <?php  echo $resultat["nom"].' '. $resultat["prenom"];  ?></p>
+                     <p>Description</p>
+                     <p><?php echo $resultat["description"];?></p>
+                </div>
+                <div class="articleDiv">
+                    <?php if($resultat["stock"]>0):?>
+                        <p>En stock</p>
+                        <a href="../controllers/PanierController.php?produit=<?= $resultat["id_produit"] ?>">Ajouter au panier</a>
+                    <?php else:?>
+                        <p>Temporairement en rupture de stock.</p>
+                    <?php endif;?>
+                </div>
+                
+            </div>
 
         <?php endforeach; ?>
     </main>
